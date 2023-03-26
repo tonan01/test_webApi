00f3e80b80e6c0889d68e2e445c6998b6e9c649a
Phạm Gia Bảo <93088813+tonan01@users.noreply.github.com>
2023-03-26 15:32:35 +0700

    Update bảo Viet Api Cơ bản

---
+++ MyWebApi/MyWebApi/Controllers/HangHoaController.cs
@@ -0,0 +1,104 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyWebApi.Controllers.Models;
+namespace MyWebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HangHoaController : ControllerBase
+    {
+        public static List<HangHoa> hangHoas = new List<HangHoa>();
+
+        #region API
+        //Lấy (hiển thị)
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            return Ok(hangHoas);//Thành công
+        }
+
+        //Lấy (hiển thị) có id
+        [HttpGet("{pId}")]
+        public IActionResult GetById(string pId)
+        {
+            try
+            {
+                var hangHoa = hangHoas.FirstOrDefault(p => p.MaHangHoa == Guid.Parse(pId));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                return Ok(hangHoa);
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        //Thêm mới
+        [HttpPost]
+        public IActionResult Create(HangHoaVM pHangHoaVM)
+        {
+            var hangHoa = new HangHoa
+            {
+                MaHangHoa = Guid.NewGuid(),
+                TenHangHoa = pHangHoaVM.TenHangHoa,
+                DonGia = pHangHoaVM.DonGia
+            };
+            hangHoas.Add(hangHoa);
+            return Ok(new
+            {
+                Success = true,
+                Data = hangHoa
+            });
+        }
+
+        //Cập nhật
+        [HttpPut("{pId}")]
+        public IActionResult Edit(string pId,HangHoa pHangHoa)
+        {
+            try
+            {
+                var hangHoa = hangHoas.FirstOrDefault(p => p.MaHangHoa == Guid.Parse(pId));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                //Update
+                if(pId!= hangHoa.MaHangHoa.ToString())
+                {
+                    return BadRequest();
+                }
+                hangHoa.TenHangHoa = pHangHoa.TenHangHoa;
+                hangHoa.DonGia= pHangHoa.DonGia;
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+
+        //Xóa
+        [HttpDelete("{pId}")]
+        public IActionResult Remote(string pId, HangHoa pHangHoa)
+        {
+            try
+            {
+                var hangHoa = hangHoas.FirstOrDefault(p => p.MaHangHoa == Guid.Parse(pId));
+                if (hangHoa == null)
+                {
+                    return NotFound();
+                }
+                hangHoas.Remove(hangHoa);
+                return Ok();
+            }
+            catch
+            {
+                return BadRequest();
+            }
+        }
+        #endregion
+    }
+}
---
+++ MyWebApi/MyWebApi/Controllers/Models/HangHoa.cs
@@ -0,0 +1,13 @@
+﻿namespace MyWebApi.Controllers.Models
+{
+    public class HangHoaVM
+    {
+        public string TenHangHoa { get; set; }
+        public double DonGia { get; set; }
+    }
+
+    public class HangHoa:HangHoaVM
+    {
+        public Guid MaHangHoa { get; set; }
+    }
+}
